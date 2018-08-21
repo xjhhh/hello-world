993d39bcf20dc7b075a0802b0f0aaeca2651128d
xjhhh <40439092+xjhhh@users.noreply.github.com>
2018-08-21 22:20:43 +0800

    Update pick_alnum_in_string.cpp

--- pick_alnum_in_string.cpp
+++ pick_alnum_in_string.cpp
@@ -6,24 +6,31 @@
 using namespace std;
 
 int main(void){
-	char str[30];
+	int s_len;
 	cout << "please input a string with length <= 20" << endl;
-	cin.get(str, 30);
-	if (strlen(str) > 20){
+	//char str[30];
+	//cin.get(str, 30);//cin.getline(str, 30);
+	//s_len = strlen(str);
+	string str;
+	getline(cin, str);
+	s_len = str.length();
+	if (s_len > 20){
 		cout << "ERROR!" << endl;
 	}
+
 	vector<char> s;
-	for (int i = 0; i < strlen(str); i++){
+	for (int i = 0; i < s_len; i++){
 		if (isalnum(str[i])){
 			if (i % 2){
 				s.push_back(str[i]);
 			}
-		} else {
-			cout << "ERROE!" << endl;
+		}
+		else {
+			cout << "ERROR!" << endl;
 			break;
 		}
-		if (i == strlen(str)){
-			for (int i = 0; i < s.size(); i++){
+		if (i == s_len - 1){
+			for (unsigned int i = 0; i < s.size(); i++){
 				cout << s[i];
 			}
 			cout << endl;
